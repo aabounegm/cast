8478fbd40824df124476d272df1481bd60bfd12f
Abdelrahman Abounegm <abounegm.abdelrahman@gmail.com>
2022-02-05 01:44:24 +0300

    Add example e2e test

---
+++ cypress/integration/example.spec.ts
@@ -0,0 +1,9 @@
+/// <reference types="cypress" />
+
+describe('App', function () {
+  it('renders App with correct footer', () => {
+    cy.visit('/');
+
+    cy.get('footer').should('contain', 'visit kit.svelte.dev to learn SvelteKit');
+  });
+});
