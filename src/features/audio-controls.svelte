055626f47eb21d99b385ff1c35a75a75c105fd46
alkaitagi <alkaitagi@outlook.com>
2022-02-05 03:39:09 +0300

    Add control buttons

--- src/features/audio-controls.svelte
+++ src/features/audio-controls.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
 </script>
 
-<div>
+<div class="flex flex-col gap-5">
   <audio controls src="/media/cc0-audio/t-rex-roar.mp3">
     Your browser does not support the
     <code>audio</code> element.
   </audio>
+  <div class="flex flex-row justify-around">
+    <button>-30s</button>
+    <button>pause</button>
+    <button>+10s</button>
+  </div>
 </div>
