572bcc53935ad26a8858a58c3848dac033529f5c
alkaitagi <alkaitagi@outlook.com>
2022-02-05 03:23:38 +0300

    Add `audio` element

---
+++ src/features/audio-controls.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  export let name: string;
+
+  let buttonText = 'Button';
+
+  function handleClick() {
+    buttonText = 'Button Clicked';
+  }
+</script>
+
+<div>
+  <audio controls src="/media/cc0-audio/t-rex-roar.mp3">
+    Your browser does not support the
+    <code>audio</code> element.
+  </audio>
+</div>
