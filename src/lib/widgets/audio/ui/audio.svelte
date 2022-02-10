5921ad9a6d662dd2d64e846b008f53bc5514c06c
VanishMax <maxkors2014@gmail.com>
2022-02-10 13:18:12 +0300

    Extend ScrubbingBar component with percentage props

--- src/lib/widgets/audio/ui/audio.svelte
+++ src/lib/widgets/audio/ui/audio.svelte
@@ -3,11 +3,26 @@
   import ScrubbingBar from './scrubbing-bar.svelte';
 
   let audioEl = null as HTMLAudioElement | null;
+  
+  let duration = 0;
+  let currentTime = 0;
+  let bufferTime = 0;
+  
+  const timeUpdate = () => {
+    if (!audioEl) return;
+    duration = audioEl.duration;
+    currentTime = audioEl.currentTime || 0;
+    if (audioEl.buffered.length) {
+      bufferTime = audioEl.buffered.end(audioEl.buffered.length - 1) || 0;
+    }
+  };
+  
   audioStore.subscribe((val) => {
     if (!audioEl) return;
     audioEl.pause();
     audioEl.src = val;
     audioEl.currentTime = 0;
+    audioEl.addEventListener('timeupdate', timeUpdate);
     audioEl.play();
   });
 </script>
@@ -15,9 +30,12 @@
 <div class="fixed bottom-0 left-0 right-0 py-8 px-10 bg-slate-800">
   <audio bind:this={audioEl} />
 
-  <ScrubbingBar />
+  <ScrubbingBar
+    bufferPercent={((bufferTime / duration) || 0) * 100}
+    percent={((currentTime / duration) || 0) * 100}
+  />
   <div class="flex justify-between">
-    <span class="text-gray-100 leading-6">13:37</span>
-    <span class="text-gray-100 leading-6">-44:37</span>
+    <span class="text-gray-100 leading-6">{currentTime}</span>
+    <span class="text-gray-100 leading-6">{duration}</span>
   </div>
 </div>
--- src/lib/widgets/audio/ui/scrubbing-bar.svelte
+++ src/lib/widgets/audio/ui/scrubbing-bar.svelte
@@ -1,15 +1,27 @@
+<script lang="ts">
+  export let percent = 0;
+  export let bufferPercent = 0;
+</script>
+
 <div class="relative w-full h-4 cursor-pointer">
   <!-- Background bar -->
   <div class="absolute inline-block left-0 top-1 w-full h-1.5 bg-slate-500 rounded-sm" />
 
   <!-- Loading (Buffer) bar -->
-  <div class="absolute inline-block left-0 top-1 w-3/4 h-1.5 bg-slate-300 rounded-sm" />
+  <div
+    style:width={`${bufferPercent}%`}
+    class="absolute inline-block left-0 top-1 w-3/4 h-1.5 bg-slate-300 rounded-sm transition"
+  />
 
   <!-- Progress bar -->
-  <div class="absolute inline-block left-0 top-1 w-1/2 h-1.5 bg-indigo-400 rounded-sm" />
+  <div
+    style:width={`${percent}%`}
+    class="absolute inline-block left-0 top-1 w-1/2 h-1.5 bg-indigo-400 rounded-sm transition"
+  />
 
   <!-- Scrubbing handle -->
   <div
-    class="absolute inline-block left-1/2 top-0 w-4 h-4 rounded-full bg-indigo-500 border border-slate-800 -translate-x-2/4"
+    style:left={`${percent}%`}
+    class="absolute inline-block top-0 w-4 h-4 rounded-full bg-indigo-500 border border-slate-800 -translate-x-2a/4 transition"
   />
 </div>
