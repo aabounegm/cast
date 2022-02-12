80222de96443b2f3d324ea9ac35a0f139b6dd347
alkaitagi <alkaitagi@outlook.com>
2022-02-12 17:40:54 +0300

    Add types

---
+++ src/podcast.ts
@@ -0,0 +1,13 @@
+export interface PodcastSeries {
+  coverUrl: string;
+  title: string;
+  authors: string;
+  website: string;
+  description: string;
+  episodesNumber: number;
+}
+export interface PodcastEpisode {
+  title: string;
+  decscription: string;
+  duration: string;
+}
