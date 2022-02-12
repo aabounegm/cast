c341a7f63a4c15bd00b614edef894cfd34d5b903
alkaitagi <alkaitagi@outlook.com>
2022-02-12 17:41:28 +0300

    Clean up

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -17,22 +17,4 @@
     margin: 0 auto;
     box-sizing: border-box;
   }
-
-  footer {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    padding: 40px;
-  }
-
-  footer a {
-    font-weight: bold;
-  }
-
-  @media (min-width: 480px) {
-    footer {
-      padding: 40px 0;
-    }
-  }
 </style>
