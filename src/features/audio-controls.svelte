f66ad15b8156239584848e384d2531918aa28234
alkaitagi <alkaitagi@outlook.com>
2022-02-15 20:58:02 +0300

    Fix AudioControls reactivity

--- src/features/audio-controls.svelte
+++ src/features/audio-controls.svelte
@@ -1,33 +1,44 @@
 <script lang="ts">
   export let audio: HTMLAudioElement;
+  export let showSeeker = false;
 
   $: percent = (100 * audio.currentTime) / audio.duration;
 
+  function play() {
+    // need this because `audio.paused` wasn't reactive otherwise
+    audio.currentTime = audio.currentTime;
+    audio.play();
+  }
+  function pause() {
+    // need this because `audio.paused` wasn't reactive otherwise
+    audio.currentTime = audio.currentTime;
+    audio.pause();
+  }
   function seek(percent: number) {
     audio.currentTime = 0.01 * percent * audio.duration;
   }
-
   function move(delta: number) {
     audio.currentTime += delta;
-    audio.play();
+    play();
   }
 </script>
 
 <div class="flex flex-col gap-5">
-  <slot />
-  <input
-    type="range"
-    min="0"
-    max="100"
-    value={percent}
-    on:input={(e) => seek(parseInt(e.currentTarget.value))}
-  />
+  {#if showSeeker}
+    <input
+      type="range"
+      min="0"
+      max="100"
+      value={percent}
+      on:input={(e) => seek(parseInt(e.currentTarget.value))}
+    />
+  {/if}
   <div class="flex flex-row justify-around">
     <button on:click={() => move(-10)} class="material-icons-round"> forward_10 </button>
     {#if audio.paused}
-      <button class="material-icons-round" on:click={() => audio.play()}> play_arrow </button>
+      <button class="material-icons-round" on:click={play}> play_arrow </button>
     {:else}
-      <button class="material-icons-round" on:click={() => audio.pause()}> pause </button>
+      <button class="material-icons-round" on:click={pause}> pause </button>
     {/if}
     <button class="material-icons-round" on:click={() => move(30)}> forward_30 </button>
   </div>
@@ -38,9 +49,12 @@
     @apply text-3xl;
   }
   button {
-    @apply bg-blue-100 text-blue-800 rounded-full w-10 h-10 duration-150 flex items-center justify-center;
+    @apply rounded-full w-10 h-10 duration-150 flex items-center justify-center;
   }
   button:hover {
-    @apply bg-blue-300;
+    @apply bg-slate-700;
+  }
+  button:active {
+    @apply bg-slate-600;
   }
 </style>
