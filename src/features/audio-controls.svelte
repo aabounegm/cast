71852aa75177096bf30c19e2fcff035b46054cb2
alkaitagi <alkaitagi@outlook.com>
2022-02-15 22:18:09 +0300

    Simplify css

--- src/features/audio-controls.svelte
+++ src/features/audio-controls.svelte
@@ -16,27 +16,14 @@
     />
   {/if}
   <div class="flex flex-row justify-around">
-    <button on:click={() => move(-10)} class="material-icons-round"> forward_10 </button>
+    <button class="material-icons-round icon-button" on:click={() => move(-10)}>
+      forward_10
+    </button>
     {#if $audio.paused}
-      <button class="material-icons-round" on:click={() => play()}> play_arrow </button>
+      <button class="material-icons-round icon-button" on:click={() => play()}> play_arrow </button>
     {:else}
-      <button class="material-icons-round" on:click={pause}> pause </button>
+      <button class="material-icons-round icon-button" on:click={pause}> pause </button>
     {/if}
-    <button class="material-icons-round" on:click={() => move(30)}> forward_30 </button>
+    <button class="material-icons-round icon-button" on:click={() => move(30)}> forward_30 </button>
   </div>
 </div>
-
-<style lang="postcss">
-  .material-icons-round {
-    @apply text-3xl;
-  }
-  button {
-    @apply rounded-full w-10 h-10 duration-150 flex items-center justify-center;
-  }
-  button:hover {
-    @apply bg-slate-700;
-  }
-  button:active {
-    @apply bg-slate-600;
-  }
-</style>
