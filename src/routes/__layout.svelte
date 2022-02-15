50e28337100918ced4c4a296912368beb00c49d0
alkaitagi <alkaitagi@outlook.com>
2022-02-15 21:32:47 +0300

    Add global audio controls component

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,7 +1,15 @@
 <script lang="ts">
   import '../app.css';
+  import AudioControls from 'src/features/audio-controls.svelte';
+  import { audio } from 'src/audio-service';
 </script>
 
 <main class="margin-auto h-full w-full max-w-md p-4 bg-slate-900 text-white">
   <slot />
+
+  <div class="fixed bottom-0 shadow-lg bg">
+    {#if audio}
+      <AudioControls {audio} />
+    {/if}
+  </div>
 </main>
