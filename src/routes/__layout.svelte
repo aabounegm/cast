686fa8d00463119b219a5b8d08caa6fdb0681f30
alkaitagi <alkaitagi@outlook.com>
2022-02-15 22:18:14 +0300

    Add global player

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -4,12 +4,20 @@
   import { audio } from 'src/audio-service';
 </script>
 
-<main class="margin-auto h-full w-full max-w-md p-4 bg-slate-900 text-white">
+<main class="relative margin-auto h-full w-full max-w-md p-4 bg-slate-900 text-white">
   <slot />
 
-  <div class="fixed bottom-0 shadow-lg bg">
-    {#if audio}
+  <div
+    class="absolute bottom-0 flex flex-row items-center shadow-lg rounded-t-xl bg-slate-700 w-max max-w-md"
+  >
+    <button class="material-icons-round icon-button">
+      <span class="material-icons-round"> video_library </span>
+    </button>
+    {#if $audio?.src}
       <AudioControls />
     {/if}
+    <button class="material-icons-round icon-button">
+      <span class="material-icons-round"> folder_shared </span>
+    </button>
   </div>
 </main>
