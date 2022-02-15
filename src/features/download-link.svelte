95f25c9c5cd2d69345975f7d71b0707c48b57c2d
alkaitagi <alkaitagi@outlook.com>
2022-02-15 20:27:40 +0300

    Add Download-Link component

---
+++ src/features/download-link.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  export let percentage: number | undefined = undefined;
+  export let onClickDownload: (() => void) | undefined = undefined;
+</script>
+
+<span class="text-sm {$$props.class}">
+  {#if !percentage}
+    <span class="a" on:click={onClickDownload}>
+      <slot>Download</slot>
+    </span>
+  {:else if percentage < 100}
+    Downloading, {percentage}%
+  {:else}
+    <span class="text-green-400"> Available offline </span>
+    <span class="text-green-400 material-icons-round !text-sm"> file_download_done </span>
+  {/if}
+</span>
