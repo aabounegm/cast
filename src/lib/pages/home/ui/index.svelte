ba4793d771f5fcf1f31104a9a234f24f36f0c1aa
Lev Chelyadinov <leva181777@gmail.com>
2022-02-17 21:20:25 +0300

    Implement the layouts of podcast lists

--- src/lib/pages/home/ui/index.svelte
+++ src/lib/pages/home/ui/index.svelte
@@ -1,5 +1,44 @@
 <script lang="ts">
   import { AudioFetch } from '$lib/features/fetch-audio';
+  import { PodcastShelf, PodcastList } from '$lib/widgets/podcasts';
+
+  const podcasts = [
+    {
+      id: 1,
+      title: 'hey',
+      author: 'hi',
+      coverUrl: 'https://placekitten.com/108/108',
+    },
+    {
+      id: 2,
+      title: 'this is a much longer title can you imagine it bruh',
+      author: 'me, mak from flushvalve',
+      coverUrl: 'https://placekitten.com/108/108',
+    },
+    {
+      id: 3,
+      title: 'this is a longwordhavinghardtoread title',
+      author: 'same person',
+      coverUrl: 'https://placekitten.com/108/108',
+    },
+    {
+      id: 4,
+      title: 'this is a longwordhavinghardtoread title',
+      author: 'same person',
+      coverUrl: 'https://placekitten.com/108/108',
+    },
+    {
+      id: 5,
+      title: 'this is a longwordhavinghardtoread title',
+      author: 'same person',
+      coverUrl: 'https://placekitten.com/108/108',
+    },
+  ];
 </script>
 
-<AudioFetch />
+<!-- <AudioFetch /> -->
+
+<div class="bg-slate-800">
+  <PodcastShelf title="Trending Now" {podcasts} />
+  <PodcastList {podcasts} />
+</div>
---
+++ src/lib/widgets/podcasts/index.ts
@@ -0,0 +1,2 @@
+export { default as PodcastShelf } from './ui/podcast-shelf.svelte';
+export { default as PodcastList } from './ui/podcast-list.svelte';
