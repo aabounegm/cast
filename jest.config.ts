c3fdd988024d8e458d257b6292bc405141739b22
Lev Chelyadinov <leva181777@gmail.com>
2022-02-17 21:28:54 +0300

    Switch from `ts-jest` to Babel for TS handling in Jest
    This is to be able to plug in additional Babel transformers, such as `svelte-inline-compile`

--- jest.config.ts
+++ jest.config.ts
@@ -3,13 +3,8 @@ import type { Config } from '@jest/types';
 const config: Config.InitialOptions = {
   verbose: true,
   transform: {
-    '^.+\\.ts$': 'ts-jest',
-    '^.+\\.svelte$': [
-      'svelte-jester',
-      {
-        preprocess: true,
-      },
-    ],
+    '\\.[jt]s$': 'babel-jest',
+    '\\.svelte$': ['svelte-jester', { preprocess: true }],
   },
   moduleFileExtensions: ['js', 'ts', 'svelte'],
   testEnvironment: 'jsdom',
