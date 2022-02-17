7acbe48bb42b06f9ec9daf65fe3898d1d81ef1a8
Lev Chelyadinov <leva181777@gmail.com>
2022-02-17 22:13:54 +0300

    Enable aliases in Jest

--- jest.config.ts
+++ jest.config.ts
@@ -1,12 +1,34 @@
+import { resolve } from 'path';
+
 import type { Config } from '@jest/types';
+
+const extensions = {
+  jsAndTs: '\\.[jt]s$',
+  svelte: '\\.svelte$',
+};
+
+/** Convert a mapping to have regular expressions as keys and values. */
+function mapDirectories(dirMapping: Record<string, string>) {
+  return Object.fromEntries(
+    Object.entries(dirMapping).map(([aliasDir, targetDir]) => [
+      `^${aliasDir.replace('$', '\\$')}/(.*)$`,
+      `${resolve(targetDir)}/$1`,
+    ])
+  );
+}
 
 const config: Config.InitialOptions = {
   verbose: true,
+
   transform: {
-    '\\.[jt]s$': 'babel-jest',
-    '\\.svelte$': ['svelte-jester', { preprocess: true }],
+    [extensions.jsAndTs]: 'babel-jest',
+    [extensions.svelte]: ['svelte-jester', { preprocess: true }],
   },
+  moduleNameMapper: mapDirectories({
+    $lib: './src/lib',
+  }),
   moduleFileExtensions: ['js', 'ts', 'svelte'],
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
   testEnvironment: 'jsdom',
 };
 
