4863f09a669d397d799eb45c1a43d15b7fbaaa4c
Lev Chelyadinov <leva181777@gmail.com>
2022-02-17 20:57:20 +0300

    Implement a generic first-level heading

---
+++ src/lib/shared/ui/h1.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import clsx from 'clsx';
+
+  let _class = '';
+  export { _class as class };
+</script>
+
+<h1
+  class={clsx('font-raleway', 'font-extrabold', 'text-lg', 'leading-6', 'text-slate-100', _class)}
+>
+  <slot />
+</h1>
