e6ceb68e3c2b0759a2dbda494270e2979760e5c9
VanishMax <maxkors2014@gmail.com>
2022-02-18 15:12:23 +0300

    Remove redundant usage of the API

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,13 +1,6 @@
 <script lang="ts">
-  import { onMount } from 'svelte';
   import { GlobalAudio } from '$lib/widgets/audio';
   import '$lib/app.css';
-  import { podcastRead } from '$lib/shared/supabase';
-  
-  // Just for the test
-  onMount(async () => {
-    await podcastRead();
-  });
 </script>
 
 <main>
