2271aabb9f8dffca717bc7f8226d5f7a806af723
alkaitagi <alkaitagi@outlook.com>
2022-02-19 08:20:40 +0300

    Improve Audio controller

--- src/entities/audio/model/audio-instance.ts
+++ src/entities/audio/model/audio-instance.ts
@@ -2,28 +2,33 @@ import { writable, get } from 'svelte/store';
 
 export const audio = writable<HTMLAudioElement>();
 
+function updateAudio(action: (a: HTMLAudioElement) => void) {
+  const a = get(audio);
+  action(a);
+  initAudio(a);
+}
+
 export function initAudio(_audio: HTMLAudioElement) {
   audio.set(_audio);
 }
 
 export function play(src?: string) {
-  const _audio = get(audio);
-  if (src) _audio.src = src;
-  _audio.play();
-  audio.set(_audio);
+  updateAudio((audio) => {
+    if (src) audio.src = src;
+    audio.play();
+  });
 }
+
 export function pause() {
-  const _audio = get(audio);
-  _audio.pause();
-  audio.set(_audio);
+  updateAudio((audio) => audio.pause());
 }
 export function seek(percent: number) {
-  const _audio = get(audio);
-  _audio.currentTime = 0.01 * percent * _audio.duration;
-  audio.set(_audio);
+  updateAudio((audio) => {
+    audio.currentTime = 0.01 * percent * audio.duration;
+  });
 }
 export function move(delta: number) {
-  const _audio = get(audio);
-  _audio.currentTime += delta;
-  play();
+  updateAudio((audio) => {
+    audio.currentTime += delta;
+  });
 }
