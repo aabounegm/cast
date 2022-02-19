5144359ecb30a91688a19a3d3255e5f06d9f9088
Lev Chelyadinov <leva181777@gmail.com>
2022-02-19 17:35:55 +0300

    Clarify the argument count in `useLocalVars`

--- src/lib/shared/lib/jest-hacks/use-local-vars.ts
+++ src/lib/shared/lib/jest-hacks/use-local-vars.ts
@@ -1,11 +1,11 @@
 /**
  * This function is just a workaround to prevent ESLint and TypeScript
- * from complaining about undefined variables. It doesn't do anything
+ * from complaining about unused variables. It doesn't do anything
  * special.
  *
  * It is meant to be used with `svelte-inline-compile` to add references
  * to variables that will be used inside the template string.
  */
-export function useLocalVars<FirstArgumentType>(first: FirstArgumentType, _rest: unknown[]) {
+export function useLocalVars<FirstArgumentType>(first: FirstArgumentType, _second: unknown[]) {
   return first;
 }
