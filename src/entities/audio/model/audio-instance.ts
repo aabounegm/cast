1ee85cb0e963fed4c987c496dae9b405e977ebc7
alkaitagi <alkaitagi@outlook.com>
2022-02-19 08:44:49 +0300

    Fix controller playback

--- src/entities/audio/model/audio-instance.ts
+++ src/entities/audio/model/audio-instance.ts
@@ -5,10 +5,10 @@ export const audio = writable<HTMLAudioElement>();
 function updateAudio(action: (a: HTMLAudioElement) => void) {
   const a = get(audio);
   action(a);
-  initAudio(a);
+  setAudio(a);
 }
 
-export function initAudio(_audio: HTMLAudioElement) {
+export function setAudio(_audio: HTMLAudioElement) {
   audio.set(_audio);
 }
 
@@ -23,9 +23,11 @@ export const pause = () => updateAudio((audio) => audio.pause());
 export const seek = (percent: number) =>
   updateAudio((audio) => {
     audio.currentTime = 0.01 * percent * audio.duration;
+    audio.play();
   });
 
 export const move = (delta: number) =>
   updateAudio((audio) => {
     audio.currentTime += delta;
+    audio.play();
   });
