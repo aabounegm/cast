5c4a7a0f29efd773e74f5a55328a2c3a6e5e75dc
Lev Chelyadinov <leva181777@gmail.com>
2022-02-21 15:44:28 +0300

    Refactor the loader service

--- src/routes/podcasts.svelte
+++ src/routes/podcasts.svelte
@@ -1,40 +1,36 @@
 <script lang="ts">
   import DownloadLink from '$lib/features/episode-presentation/ui/download-link.svelte';
   import EpisodeCard from '$lib/features/episode-presentation/ui/episode-card.svelte';
-  import { getEpisodes, getPodcast } from 'src/loader-service';
-  import type { PodcastEpisode, PodcastSeries } from 'src/podcast';
+  import { play } from '$lib/entities/audio/model/audio-instance';
+  import { getPodcast } from '$lib/entities/podcast';
+  import type { Podcast } from '$lib/shared/api';
 
-  import { audio, play } from '$lib/entities/audio/model/audio-instance';
-
-  let podcast: PodcastSeries | undefined;
-  let episodes: PodcastEpisode[] | undefined;
+  let podcast: Podcast | undefined;
 
   $: {
-    let id = '23424';
+    let id = 23424;
     getPodcast(id).then((r) => (podcast = r));
-    getEpisodes(id).then((r) => (episodes = r));
   }
 </script>
 
-{#if podcast && episodes}
+{#if podcast}
   <div class="flex flex-row gap-4 p-4">
     <img class="w-24 h-24 rounded-xl" src={podcast.coverUrl} alt="cover" />
     <div class="flex flex-col gap-2 text-sm">
       <h3 class="text-xl font-bold">{podcast.title}</h3>
-      <p>{podcast.authors}</p>
+      <p>{podcast.author}</p>
       <p>
-        {podcast.episodesNumber} episodes /
+        {podcast.episodes.length} episodes /
         <DownloadLink>Download all</DownloadLink>
       </p>
     </div>
   </div>
 
   <div class="flex flex-col p-4 gap-4 items-stretch">
-    {#each episodes as episode}
+    {#each podcast.episodes as episode}
       <EpisodeCard
         {episode}
-        selected={$audio.src == episode.src}
-        onClickPlay={() => play(episode.src)}
+        onClickPlay={() => play(episode.audioUrl)}
       />
     {/each}
   </div>
