13e0972d1de8738fe8f45f18227dd2673d689af4
Lev Chelyadinov <leva181777@gmail.com>
2022-02-21 23:13:45 +0300

    Allow specifying an alternative text for the download link

--- src/lib/features/download-episode/ui/download-link.svelte
+++ src/lib/features/download-episode/ui/download-link.svelte
@@ -1,3 +1,10 @@
-<span class="text-sm a" on:click>
+<script lang="ts">
+  export let downloadAll = false;
+</script>
+
+<span class="text-indigo-300 hover:text-indigo-200 underline underline-offset-2 cursor-pointer" on:click>
   Download
+  {#if downloadAll}
+    all
+  {/if}
 </span>
