b46b190de74dbbb21de1f403b4ef36534b1827e1
Lev Chelyadinov <leva181777@gmail.com>
2022-02-21 16:05:00 +0300

    Support the paused state in the play button

--- src/lib/features/playback-controls/ui/card-playback-button.svelte
+++ src/lib/features/playback-controls/ui/card-playback-button.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
   import IconPlayArrow from '~icons/ic/round-play-arrow';
+  import IconPause from '~icons/ic/round-pause';
   import { IconButton } from "$lib/shared/ui";
+
+  export let playing = false;
 </script>
 
-<IconButton icon={IconPlayArrow} on:click />
+<IconButton icon={playing ? IconPause : IconPlayArrow} on:click />
