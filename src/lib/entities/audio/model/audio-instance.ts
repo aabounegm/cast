896aaec401ae935e748e7526a1ee711dc8656173
Lev Chelyadinov <leva181777@gmail.com>
2022-02-21 13:41:39 +0300

    Refactor the audio model to use native Svelte APIs

--- src/lib/entities/audio/model/audio-instance.ts
+++ src/lib/entities/audio/model/audio-instance.ts
@@ -1,33 +1,37 @@
-import { writable, get } from 'svelte/store';
+import { writable } from 'svelte/store';
 
 export const audio = writable<HTMLAudioElement>();
 
-function updateAudio(action: (a: HTMLAudioElement) => void) {
-  const el = get(audio);
-  action(el);
-  setAudio(el);
-}
+export const play = (src?: string) => {
+  audio.update(($audio) => {
+    if (src) {
+      $audio.src = src;
+    }
 
-export function setAudio(_audio: HTMLAudioElement) {
-  audio.set(_audio);
-}
-
-export const play = (src?: string) =>
-  updateAudio((audio) => {
-    if (src) audio.src = src;
-    audio.play();
+    $audio.play();
+    return $audio;
   });
+};
 
-export const pause = () => updateAudio((audio) => audio.pause());
-
-export const seek = (percent: number) =>
-  updateAudio((audio) => {
-    audio.currentTime = 0.01 * percent * audio.duration;
-    audio.play();
+export const pause = () => {
+  audio.update(($audio) => {
+    $audio.pause();
+    return $audio;
   });
+};
 
-export const move = (delta: number) =>
-  updateAudio((audio) => {
-    audio.currentTime += delta;
-    audio.play();
+export const seek = (percent: number) => {
+  audio.update(($audio) => {
+    $audio.currentTime = 0.01 * percent * $audio.duration;
+    $audio.play();
+    return $audio;
   });
+};
+
+export const move = (delta: number) => {
+  audio.update(($audio) => {
+    $audio.currentTime += delta;
+    $audio.play();
+    return $audio;
+  });
+};
--- src/lib/features/playback-controls/ui/controls.svelte
+++ src/lib/features/playback-controls/ui/controls.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-  import { play, pause, seek, move, audio } from '$lib/entities/audio/model/audio-instance';
+  import { play, pause, seek, move, audio } from '$lib/entities/audio';
+
   export let showSeeker = false;
   $: percent = (100 * $audio.currentTime) / $audio.duration;
 </script>
