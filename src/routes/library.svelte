040946094b806215576355d41f4edb075800e3ca
Abdelrahman Abounegm <abounegm.abdelrahman@gmail.com>
2022-02-23 02:24:36 +0300

    Add basic favorites section to the library page

--- src/routes/library.svelte
+++ src/routes/library.svelte
@@ -1,5 +1,19 @@
+<script lang="ts">
+  import { H1 } from '$lib/shared/ui';
+  import { likesStore } from '$lib/features/like-episode';
+  import { EpisodeCard } from '$lib/widgets/episode';
+
+  $: episodes = Array.from($likesStore.values());
+</script>
+
 <!-- Sign in card -->
 
 <!-- Favorites -->
+<section class="flex flex-col p-4 gap-4 items-stretch">
+  <H1>Favorites</H1>
+  {#each episodes as episode (episode.id)}
+    <EpisodeCard {episode} playing={false} />
+  {/each}
+</section>
 
 <!-- Offline -->
