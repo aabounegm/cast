ad3a3fad2c6af3b96dec1c0075bf957e50ed12e5
Lev Chelyadinov <leva181777@gmail.com>
2022-02-22 11:52:19 +0300

    Move the specs in a separate folder

--- src/lib/widgets/podcasts/ui/specs/podcast-shelf.spec.ts
+++ src/lib/widgets/podcasts/ui/specs/podcast-shelf.spec.ts
@@ -1,7 +1,7 @@
 import svelte from 'svelte-inline-compile';
 import { render } from '@testing-library/svelte';
 
-import { PodcastList } from '..';
+import { PodcastShelf } from '../..';
 import { useLocalVars } from '$lib/shared/lib/jest-hacks';
 import type { Podcast } from '$lib/shared/api';
 
@@ -24,7 +24,7 @@ const podcasts: Podcast[] = [
 
 it('renders all podcasts', () => {
   const { getByText } = render(
-    useLocalVars(svelte`<PodcastList {podcasts} />`, [PodcastList, podcasts])
+    useLocalVars(svelte`<PodcastShelf {podcasts} />`, [PodcastShelf, podcasts])
   );
 
   for (const podcast of podcasts) {
