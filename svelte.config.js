bee461fc7338153e78d6e624b02ee4f656bf980e
Lev Chelyadinov <leva181777@gmail.com>
2022-02-22 11:29:05 +0300

    Remove the `src` alias

--- svelte.config.js
+++ svelte.config.js
@@ -1,25 +1,14 @@
-import * as path from 'path';
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 import icons from 'unplugin-icons/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://github.com/sveltejs/svelte-preprocess
-  // for more information about preprocessors
   preprocess: preprocess(),
 
   kit: {
     adapter: adapter(),
-
-    // hydrate the <div id="svelte"> element in src/app.html
-    // target: '#svelte',
     vite: {
-      resolve: {
-        alias: {
-          src: path.resolve('./src'),
-        },
-      },
       plugins: [icons({ compiler: 'svelte' })],
     },
   },
