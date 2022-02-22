29771ce62a2c423a48897245e838a2ff027d2818
Abdelrahman Abounegm <abounegm.abdelrahman@gmail.com>
2022-02-23 02:02:54 +0300

    Add support for IconButton to be a link

--- src/lib/shared/ui/icon-button.svelte
+++ src/lib/shared/ui/icon-button.svelte
@@ -4,15 +4,21 @@
 
   let _class = '';
   export { _class as class };
+  export let href = '';
   export let icon: typeof SvelteComponent;
-</script>
 
-<button
-  class={clsx(
+  $: classes = clsx(
     'text-xl flex items-center justify-center w-8 h-8 rounded-full hover:bg-slate-600 active:bg-slate-500',
     _class
-  )}
-  on:click
->
-  <svelte:component this={icon} />
-</button>
+  );
+</script>
+
+{#if href}
+  <a {href} class={classes}>
+    <svelte:component this={icon} />
+  </a>
+{:else}
+  <button class={classes} on:click>
+    <svelte:component this={icon} />
+  </button>
+{/if}
