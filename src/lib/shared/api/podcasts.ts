aa448e356b64bbf1f0fed7e47c6777a3834dadef
alkaitagi <alkaitagi@outlook.com>
2022-02-28 10:36:01 +0300

    Remove logs

--- src/lib/shared/api/podcasts.ts
+++ src/lib/shared/api/podcasts.ts
@@ -5,7 +5,6 @@ import supabaseClient from './supabase';
 export const podcastList = async (): Promise<Podcast[]> => {
   const res = await supabaseClient.from<SBPodcast>('podcasts').select('*, episodes (*)');
   if (res.error) {
-    alert(res);
     return [];
   }
 
@@ -20,7 +19,6 @@ export const podcastGet = async (id: number): Promise<null | Podcast> => {
     .single();
 
   if (res.error || !res.data) {
-    alert(res);
     return null;
   }
 
