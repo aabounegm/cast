45277a799e7a0df5c659be55848c8f37d37e7404
Khaled Ismaeel <k.ismaeel@innopolis.university>
2022-03-01 21:36:32 +0300

    Add complete `now-playing` route

---
+++ src/lib/features/read-transcript/index.ts
@@ -0,0 +1,1 @@
+export {default as ToggleTranscript} from './ui/toggle-transcript.svelte';
---
+++ src/lib/features/read-transcript/ui/toggle-transcript.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import IconSubtitles from '~icons/ic/twotone-subtitles';
+  import IconSubtitlesOff from '~icons/ic/twotone-subtitles-off';
+
+  export let transcriptShown: boolean;
+</script>
+
+<button
+  class="flex items-center py-2 px-3 gap-2 text-sm font-extrabold bg-slate-100 bg-opacity-5 rounded"
+  on:click
+>
+  {#if transcriptShown}
+    <IconSubtitlesOff class="w-6 h-6" />
+    Hide the transcript
+  {:else}
+    <IconSubtitles class="w-6 h-6" />
+    Read the transcript
+  {/if}
+</button>
