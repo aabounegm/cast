01786626818eba99cd06da98e8e8fec33d874381
Abdelrahman Abounegm <abounegm.abdelrahman@gmail.com>
2022-03-05 03:26:14 +0300

    Add prop to display name next to icon in button

--- src/lib/shared/ui/icon-button.svelte
+++ src/lib/shared/ui/icon-button.svelte
@@ -5,24 +5,26 @@
   let _class = '';
   export { _class as class };
   export let href = '';
+  export let showLabel = false;
   export let icon: typeof SvelteComponent;
   export let iconClass = '';
   export let name: string;
 
   $: classes = clsx(
-    'text-xl flex items-center justify-center w-8 h-8 rounded-full hover:bg-slate-600 active:bg-slate-500',
+    'text-s flex items-center justify-center w-8 h-8 rounded-full hover:bg-slate-600 active:bg-slate-500',
     _class
   );
 </script>
 
+<!-- TODO: remove the duplication somehow -->
 {#if href}
   <a {href} class={classes} {...$$restProps} title={name}>
     <svelte:component this={icon} class={iconClass} />
-    <span class="sr-only">{name}</span>
+    <span class:sr-only={!showLabel} class="ml-2">{name}</span>
   </a>
 {:else}
   <button class={classes} on:click {...$$restProps} title={name}>
     <svelte:component this={icon} class={iconClass} />
-    <span class="sr-only">{name}</span>
+    <span class:sr-only={!showLabel} class="ml-2">{name}</span>
   </button>
 {/if}
