d3c2b5c19f457b167befe0b90b3fa4b74a9c4d8c
Abdelrahman Abounegm <abounegm.abdelrahman@gmail.com>
2022-03-05 03:27:24 +0300

    Indicate the currently active page by text color

--- src/lib/shared/ui/icon-button.svelte
+++ src/lib/shared/ui/icon-button.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { page } from '$app/stores';
   import clsx from 'clsx';
   import type { SvelteComponent } from 'svelte';
 
@@ -12,7 +13,8 @@
 
   $: classes = clsx(
     'text-s flex items-center justify-center w-8 h-8 rounded-full hover:bg-slate-600 active:bg-slate-500',
-    _class
+    _class,
+    href && $page.url.href.includes(href) && 'text-indigo-300'
   );
 </script>
 
