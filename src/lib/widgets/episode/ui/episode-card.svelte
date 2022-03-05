c76e3493e8a83c63a86826f8320ae5516c3a644c
VanishMax <maxkors2014@gmail.com>
2022-03-05 10:18:41 +0500

    Implement EpisodeCard audio playing state

--- src/lib/widgets/episode/ui/episode-card.svelte
+++ src/lib/widgets/episode/ui/episode-card.svelte
@@ -3,15 +3,25 @@
   import { LikeButton } from '$lib/features/like-episode';
   import { DownloadLink } from '$lib/features/download-episode';
   import { EpisodeCardShell } from '$lib/entities/episode';
+  import { play, pause, audio } from '$lib/entities/audio';
   import type { Episode } from '$lib/shared/api';
 
   export let episode: Episode;
-  export let playing: boolean;
+
+  $: playing = $audio?.src === episode.audioUrl && !$audio?.paused && !$audio?.ended;
+
+  const playAudio = () => {
+    if (playing) {
+      pause();
+    } else {
+      play(episode.audioUrl);
+    }
+  };
 </script>
 
 <EpisodeCardShell {episode}>
   <svelte:fragment slot="play">
-    <PlaybackButton {playing} />
+    <PlaybackButton {playing} on:click={playAudio} />
   </svelte:fragment>
 
   <svelte:fragment slot="download">
