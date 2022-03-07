abfb1b40dd7e78ca008f633bec578a52387fd5c7
Lev Chelyadinov <leva181777@gmail.com>
2022-03-07 12:59:03 +0300

    Refactor the API code to match the new schema

--- src/lib/shared/api/index.ts
+++ src/lib/shared/api/index.ts
@@ -1,4 +1,3 @@
 export { default as supabaseClient } from './supabase';
 export { podcastList, podcastGet } from './podcasts';
-export { episodeList, episodeGet } from './episodes';
 export type { Episode, Podcast } from './types';
