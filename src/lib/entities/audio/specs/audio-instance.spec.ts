f48126e685816a0acb6b522cdc1c801ec5ea28b9
alkaitagi <alkaitagi@outlook.com>
2022-03-08 00:23:23 +0300

    Add audio API specs

---
+++ src/lib/entities/audio/specs/audio-instance.spec.ts
@@ -0,0 +1,31 @@
+import { get } from 'svelte/store';
+import { src, currentTime, paused, move, pause, play, seek } from '..';
+
+describe('Audio playback API', () => {
+  const defaultSrc = 'http://commondatastorage.googleapis.com/codeskulptor-assets/Evillaugh.ogg';
+  const playDefault = () => play(defaultSrc);
+
+  it('load audio', () => {
+    playDefault();
+    expect(get(src)).toEqual(defaultSrc);
+    expect(get(paused)).toBeFalsy();
+    pause();
+  });
+
+  it('pause playback', () => {
+    playDefault();
+    pause();
+    play();
+    expect(get(paused)).toBeFalsy();
+    pause();
+    expect(get(paused)).toBeTruthy();
+  });
+
+  it('control playback current time', () => {
+    playDefault();
+    seek(0.5);
+    move(2);
+    expect(get(currentTime)).toEqual(2.5);
+    pause();
+  });
+});
