773e68cbdedba4dd045d37a4d82c7428eeac63ee
Abdelrahman Abounegm <abounegm.abdelrahman@gmail.com>
2022-03-09 00:29:49 +0300

    Add skeleton tests to be implemented later

---
+++ src/lib/features/playback-controls/ui/specs/full-controls.spec.ts
@@ -0,0 +1,9 @@
+it.todo('displays no control buttons when no audio is loaded');
+
+it.todo('displays pause button when audio is loaded but not playing');
+
+it.todo('displays play button when audio is loaded and playing');
+
+it.todo('displays rewind & fast forward buttons when audio is loaded');
+
+export {};
