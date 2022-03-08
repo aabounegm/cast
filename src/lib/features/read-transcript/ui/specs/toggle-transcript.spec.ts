d358c7cc6ae402f35414ac141371e57d7f7d15e3
Khaled Ismaeel <k.ismaeel@innopolis.university>
2022-03-08 11:49:38 +0300

    Add empty specs

---
+++ src/lib/features/read-transcript/ui/specs/toggle-transcript.spec.ts
@@ -0,0 +1,5 @@
+export {};
+
+it.todo('displays the current transcript state to the user');
+
+it.todo('dispatches the click event to the handler');
---
+++ src/lib/widgets/now-playing/ui/specs/now-playing.spec.ts
@@ -0,0 +1,5 @@
+export {};
+
+it.todo('initially contains episode display');
+
+it.todo('toggles between episode display and transcript');
