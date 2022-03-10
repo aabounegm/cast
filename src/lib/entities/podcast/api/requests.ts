6ada524032d812bde733182a616707c66556ab6f
Lev Chelyadinov <leva181777@gmail.com>
2022-03-10 18:46:54 +0300

    Implement fetching podcasts by ID
    Co-authored-by: Abdelrahman Abounegm <abounegm.abdelrahman@gmail.com>

--- src/lib/entities/podcast/api/requests.ts
+++ src/lib/entities/podcast/api/requests.ts
@@ -5,6 +5,7 @@ import {
   supabaseClient,
   notNull,
 } from '$lib/shared/api';
+import { getPodcastByID } from '../model/store';
 
 const allFieldsWithEpisodes = `
   id,
@@ -31,6 +32,11 @@ export const podcastList = async (): Promise<Podcast[]> => {
 };
 
 export const podcastGet = async (id: number): Promise<null | Podcast> => {
+  const cachedPodcast = getPodcastByID(id);
+  if (cachedPodcast !== undefined) {
+    return cachedPodcast;
+  }
+
   const res = await supabaseClient
     .from<SBPodcast>('podcasts')
     .select(allFieldsWithEpisodes)
--- src/lib/entities/podcast/model/store.ts
+++ src/lib/entities/podcast/model/store.ts
@@ -1,4 +1,8 @@
-import { writable } from 'svelte/store';
+import { writable, get } from 'svelte/store';
 import type { Podcast } from '$lib/shared/api';
 
 export const podcasts = writable<Podcast[]>([]);
+
+export function getPodcastByID(thisID: Podcast['id']) {
+  return get(podcasts).find((podcast) => podcast.id === thisID);
+}
