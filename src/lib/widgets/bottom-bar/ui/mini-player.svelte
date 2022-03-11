ae802b88772027aab521a9b437b734caeee86c6c
Abdelrahman Abounegm <abounegm.abdelrahman@gmail.com>
2022-03-11 03:00:38 +0300

    Control visibility of "now playing" via mini bar

--- src/lib/widgets/bottom-bar/ui/mini-player.svelte
+++ src/lib/widgets/bottom-bar/ui/mini-player.svelte
@@ -6,11 +6,24 @@
   import { move, paused } from '$lib/entities/audio';
   import { currentlyPlayingEpisode } from '$lib/entities/episode';
   import { getPodcastByID } from '$lib/entities/podcast';
+  import { NowPlaying } from '$lib/widgets/now-playing';
 
-  $: podcast = getPodcastByID($currentlyPlayingEpisode!.podcastID);
+  $: episode = $currentlyPlayingEpisode!;
+  $: podcast = getPodcastByID(episode.podcastID)!;
+
+  let nowPlayingActive = false;
 </script>
 
-<div class="flex justify-evenly grow-[0.5] cursor-pointer rounded-lg bg-slate-800 py-2">
+{#if nowPlayingActive}
+  <NowPlaying {podcast} {episode} on:minimize={() => (nowPlayingActive = false)} />
+{/if}
+
+<div
+  class="flex justify-evenly grow-[0.5] cursor-pointer rounded-lg bg-slate-800 py-2"
+  on:click={() => {
+    nowPlayingActive = true;
+  }}
+>
   <img src={podcast?.coverUrl} width="40" alt="" />
   <div class="flex justify-between">
     <IconButton
@@ -18,15 +31,27 @@
       icon={IconReplay10}
       class="w-9 h-9"
       iconClass="w-7 h-7"
-      on:click={() => move(-10)}
+      on:click={(e) => {
+        e.stopPropagation();
+        move(-10);
+      }}
     />
-    <PlaybackButton playing={!$paused} on:click={() => toggleGlobalPlayback()} />
+    <PlaybackButton
+      playing={!$paused}
+      on:click={(e) => {
+        e.stopPropagation();
+        toggleGlobalPlayback();
+      }}
+    />
     <IconButton
       name="Skip forward 30 seconds"
       icon={IconForward30}
       class="w-9 h-9"
       iconClass="w-7 h-7"
-      on:click={() => move(30)}
+      on:click={(e) => {
+        e.stopPropagation();
+        move(30);
+      }}
     />
   </div>
 </div>
--- src/lib/widgets/now-playing/ui/now-playing.svelte
+++ src/lib/widgets/now-playing/ui/now-playing.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { fly } from 'svelte/transition';
   import { FullControls } from '$lib/features/playback-controls';
   import IconArrowDown from '~icons/ic/round-keyboard-arrow-down';
   import { IconButton } from '$lib/shared/ui';
@@ -6,6 +7,9 @@
   import { ToggleTranscript } from '$lib/features/read-transcript';
   import type { Episode, Podcast } from '$lib/shared/api';
   import Transcript from './transcript.svelte';
+  import { createEventDispatcher } from 'svelte';
+
+  const dispatch = createEventDispatcher<{ minimize: void }>();
 
   export let podcast: Podcast;
   export let episode: Episode;
@@ -13,7 +17,10 @@
   let transcriptShown = false;
 </script>
 
-<div class="bg-black bg-opacity-20 pt-4 flex flex-col justify-end absolute bottom-0">
+<div
+  class="bg-black bg-opacity-20 pt-4 flex flex-col justify-end absolute bottom-0"
+  transition:fly={{ y: 100 }}
+>
   <div class="bg-slate-800 flex flex-col rounded-t-2xl py-10 px-8 items-center">
     <div class="flex flex-col items-center relative">
       {#if transcriptShown}
@@ -26,7 +33,13 @@
     <FullControls class="w-full mt-5" />
     <div class="w-full mt-4 flex items-center justify-between">
       <ToggleTranscript {transcriptShown} on:click={() => (transcriptShown = !transcriptShown)} />
-      <IconButton name="Collapse" icon={IconArrowDown} class="w-10 h-10" iconClass="w-8 h-8" />
+      <IconButton
+        name="Collapse"
+        icon={IconArrowDown}
+        class="w-10 h-10"
+        iconClass="w-8 h-8"
+        on:click={() => dispatch('minimize')}
+      />
     </div>
   </div>
 </div>
