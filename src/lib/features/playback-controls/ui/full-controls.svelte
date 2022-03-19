af67342bf9326a85c227ba6d569f8f76d4522ddf
alkaitagi <alkaitagi@outlook.com>
2022-03-19 11:32:43 +0300

    Take playback controls out of full controls

--- src/lib/features/playback-controls/ui/full-controls.svelte
+++ src/lib/features/playback-controls/ui/full-controls.svelte
@@ -1,21 +1,8 @@
 <script lang="ts">
-  import IconReplay10 from '~icons/ic/round-replay-10';
-  import IconForward30 from '~icons/ic/round-forward-30';
-  import {
-    seek,
-    move,
-    currentTime,
-    duration,
-    paused,
-    buffered,
-    pause,
-    play,
-  } from '$lib/entities/audio';
-  import { IconButton } from '$lib/shared/ui/';
-  import PlaybackButton from './playback-button.svelte';
-  import { toggleGlobalPlayback } from '../model/toggle-global-playback';
+  import { seek, currentTime, duration, paused, buffered, pause, play } from '$lib/entities/audio';
   import ScrubbingBar from './scrubbing-bar.svelte';
   import clsx from 'clsx';
+  import PlaybackControlButtons from './playback-control-buttons.svelte';
 
   let _class = '';
   export { _class as class };
@@ -32,21 +19,5 @@
       seek(e.detail.position);
     }}
   />
-  <div class="flex flex-row items-center justify-between">
-    <IconButton
-      name="Replay the last 10 seconds"
-      icon={IconReplay10}
-      class="w-9 h-9"
-      iconClass="w-7 h-7"
-      on:click={() => move(-10)}
-    />
-    <PlaybackButton prominent playing={!$paused} on:click={() => toggleGlobalPlayback()} />
-    <IconButton
-      name="Skip forward 30 seconds"
-      icon={IconForward30}
-      class="w-9 h-9"
-      iconClass="w-7 h-7"
-      on:click={() => move(30)}
-    />
-  </div>
+  <PlaybackControlButtons />
 </div>
---
+++ src/lib/features/playback-controls/ui/playback-control-buttons.svelte
@@ -0,0 +1,34 @@
+<script>
+  import { paused, move } from '$lib/entities/audio';
+  import { toggleGlobalPlayback } from '..';
+  import { IconButton } from '$lib/shared/ui/';
+  import PlaybackButton from './playback-button.svelte';
+  import IconReplay10 from '~icons/ic/round-replay-10';
+  import IconForward30 from '~icons/ic/round-forward-30';
+</script>
+
+<div class="flex flex-row items-center justify-between">
+  <slot name="pre" />
+  <IconButton
+    name="Replay the last 10 seconds"
+    icon={IconReplay10}
+    class="w-9 h-9"
+    iconClass="w-7 h-7"
+    on:click={(e) => {
+      e.stopPropagation();
+      move(-10);
+    }}
+  />
+  <PlaybackButton prominent playing={!$paused} on:click={() => toggleGlobalPlayback()} />
+  <IconButton
+    name="Skip forward 30 seconds"
+    icon={IconForward30}
+    class="w-9 h-9"
+    iconClass="w-7 h-7"
+    on:click={(e) => {
+      e.stopPropagation();
+      move(30);
+    }}
+  />
+  <slot name="post" />
+</div>
