1b76fead2f82da176050bc8a68c72a520ecf7633
alkaitagi <alkaitagi@outlook.com>
2022-03-19 11:34:11 +0300

    Use playback controls in mini player

--- src/lib/widgets/bottom-bar/ui/mini-player.svelte
+++ src/lib/widgets/bottom-bar/ui/mini-player.svelte
@@ -1,12 +1,8 @@
 <script lang="ts">
-  import IconReplay10 from '~icons/ic/round-replay-10';
-  import IconForward30 from '~icons/ic/round-forward-30';
-  import { PlaybackButton, toggleGlobalPlayback } from '$lib/features/playback-controls';
-  import { IconButton } from '$lib/shared/ui';
-  import { move, paused } from '$lib/entities/audio';
   import { currentlyPlayingEpisode } from '$lib/entities/episode';
   import { getPodcastByID } from '$lib/entities/podcast';
   import { NowPlaying } from '$lib/widgets/now-playing';
+  import PlaybackControlButtons from '$lib/features/playback-controls/ui/playback-control-buttons.svelte';
 
   $: episode = $currentlyPlayingEpisode!;
   $: podcast = getPodcastByID(episode.podcastID)!;
@@ -25,33 +21,5 @@
   }}
 >
   <img src={podcast?.coverUrl} width="40" alt="" />
-  <div class="flex justify-between">
-    <IconButton
-      name="Replay the last 10 seconds"
-      icon={IconReplay10}
-      class="w-9 h-9"
-      iconClass="w-7 h-7"
-      on:click={(e) => {
-        e.stopPropagation();
-        move(-10);
-      }}
-    />
-    <PlaybackButton
-      playing={!$paused}
-      on:click={(e) => {
-        e.stopPropagation();
-        toggleGlobalPlayback();
-      }}
-    />
-    <IconButton
-      name="Skip forward 30 seconds"
-      icon={IconForward30}
-      class="w-9 h-9"
-      iconClass="w-7 h-7"
-      on:click={(e) => {
-        e.stopPropagation();
-        move(30);
-      }}
-    />
-  </div>
+  <PlaybackControlButtons />
 </div>
