f5497da26de151ec34a0d84d5504364a30e28933
Lev Chelyadinov <leva181777@gmail.com>
2022-03-19 22:13:26 +0300

    Implement the skeleton text for the transcript

---
+++ src/lib/features/read-transcript/ui/skeleton-transcript.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { random, range } from 'lodash-es';
+
+  export let lines: number;
+</script>
+
+<div class="animate-pulse flex flex-col gap-2">
+  {#each range(lines) as _lineIndex}
+    <div class="h-4 bg-slate-400 rounded" style:width={`${random(50, 100)}%`} />
+  {/each}
+</div>
