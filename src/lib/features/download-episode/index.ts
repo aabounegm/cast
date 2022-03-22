9307ebc5cfb65dd37a68a9c772d65973b4d07328
Abdelrahman Abounegm <abounegm.abdelrahman@gmail.com>
2022-03-22 18:17:53 +0300

    Add component to wrap the possible download states

--- src/lib/features/download-episode/index.ts
+++ src/lib/features/download-episode/index.ts
@@ -1,3 +1,4 @@
 export { default as DownloadLink } from './ui/download-link.svelte';
 export { default as DownloadProgress } from './ui/download-progress.svelte';
 export { default as DownloadDone } from './ui/download-done.svelte';
+export { default as DownloadLinkWithProgress } from './ui/download-link-with-progress.svelte';
---
+++ src/lib/features/download-episode/ui/download-link-with-progress.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  import { derived, readable } from 'svelte/store';
+  import DownloadDone from './download-done.svelte';
+  import DownloadLink from './download-link.svelte';
+  import DownloadProgress from './download-progress.svelte';
+  import { startEpisodeDownload } from '../lib/download';
+
+  export let urls: string[];
+  export let downloadAll = false;
+
+  let progress = readable<number | null>(null);
+
+  $: {
+    if (!downloadAll && urls.length !== 1)
+      console.error('Expected 1 URL for downloading a single episode');
+  }
+
+  function startDownloads() {
+    progress = derived(
+      urls.map(startEpisodeDownload),
+      (downloads) => {
+        const sum = downloads.reduce((partialSum, a) => partialSum + a, 0);
+        return sum / downloads.length;
+      },
+      0
+    );
+  }
+</script>
+
+{#if $progress === null}
+  <DownloadLink on:click={startDownloads} {downloadAll} />
+{:else if $progress < 100}
+  <DownloadProgress percentage={$progress} />
+{:else}
+  <DownloadDone />
+{/if}
