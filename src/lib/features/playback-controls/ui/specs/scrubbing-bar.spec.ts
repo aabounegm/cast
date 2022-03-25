12889593b78517da623bcc7c9a4a274c9bba57ba
alkaitagi <alkaitagi@outlook.com>
2022-03-25 15:57:29 +0300

    Fix scrubbing bar tests

--- src/lib/features/playback-controls/ui/specs/scrubbing-bar.spec.ts
+++ src/lib/features/playback-controls/ui/specs/scrubbing-bar.spec.ts
@@ -1,24 +1,11 @@
 import svelte from 'svelte-inline-compile';
 import { render, fireEvent } from '@testing-library/svelte';
 
-import { seek, currentTime, duration } from '$lib/entities/audio';
-
 import { useLocalVars } from '$lib/shared/lib/jest-hacks';
 import ScrubbingBar from '../scrubbing-bar.svelte';
 
 const sampleDuration = 13 * 60 + 37; // 13:37
 const samplePosition = 1 * 60 + 17; // 1:17
-
-jest.mock('$lib/entities/audio', () => ({
-  ...jest.requireActual('$lib/entities/audio'),
-  duration: {
-    subscribe: jest.fn(() => jest.fn()),
-  },
-  currentTime: {
-    subscribe: jest.fn(() => jest.fn()),
-  },
-  seek: jest.fn().mockName('seek() from $lib/entities/audio'),
-}));
 
 it('reflects the current audio position in the scrubbing thumb', () => {
   const { getByRole } = render(ScrubbingBar, {
@@ -60,45 +47,4 @@ it('dispatches a `scrub` event upon scrubbing', () => {
   fireEvent.input(getByRole('slider'), { target: { value: newValue } });
 
   expect(scrubHandler).toBeCalledWith(new CustomEvent('scrub', { detail: { position: newValue } }));
-});
-
-it('listens to the global playback position and audio duration', () => {
-  const samplePosition = 13 * 60 + 37; // 13:37
-  const sampleDuration = 20 * 60; // 20:00
-
-  jest.mocked(currentTime.subscribe).mockImplementation((subscriber) => {
-    subscriber(samplePosition);
-    return jest.fn();
-  });
-  jest.mocked(duration.subscribe).mockImplementation((subscriber) => {
-    subscriber(sampleDuration);
-    return jest.fn();
-  });
-
-  const { getByText } = render(ScrubbingBar);
-
-  expect(getByText('13:37')).toBeInTheDocument();
-  expect(getByText('-6:23')).toBeInTheDocument(); // 20:00 - 13:37 = 6:23
-});
-
-it('seeks the global audio on scrubbing', async () => {
-  const samplePosition = 13 * 60 + 37; // 13:37
-  const sampleDuration = 20 * 60; // 20:00
-
-  jest.mocked(currentTime.subscribe).mockImplementation((subscriber) => {
-    subscriber(samplePosition);
-    return jest.fn();
-  });
-  jest.mocked(duration.subscribe).mockImplementation((subscriber) => {
-    subscriber(sampleDuration);
-    return jest.fn();
-  });
-
-  const { getByRole } = render(ScrubbingBar);
-
-  // Drag the scrubbing bar from 13:37 to 13:38
-  const newValue = 13 * 60 + 38;
-  fireEvent.input(getByRole('slider'), { target: { value: newValue } });
-
-  expect(seek).toHaveBeenCalledWith(newValue);
 });
