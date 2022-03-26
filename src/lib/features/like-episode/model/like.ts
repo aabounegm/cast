73f5bb09a1949140d0b199da7a9efcb975c44fc8
Khaled Ismaeel <k.ismaeel@innopolis.university>
2022-03-26 16:03:25 +0300

    Add sign in snackbar

--- src/lib/features/like-episode/model/like.ts
+++ src/lib/features/like-episode/model/like.ts
@@ -1,5 +1,12 @@
 import type { Episode } from '$lib/shared/api';
-import { persistentWritable, localStorageAdapter } from 'svelte-persistent-writable';
+import {
+  persistentWritable,
+  localStorageAdapter,
+  sessionStorageAdapter,
+} from 'svelte-persistent-writable';
+import { supabaseClient } from '$lib/shared/api';
+import { snackbar } from '$lib/shared/ui/snackbar';
+import { get } from 'svelte/store';
 
 /**
  * The store containing set of episodes liked by the current user.
@@ -36,12 +43,29 @@ export function removeLike(episode: Episode) {
   });
 }
 
+const hasReceivedSignInSnackbarStore = persistentWritable(false, {
+  storage: sessionStorageAdapter('hasReceivedSignInSnackbar'),
+});
+
 /**
  * Toggles the like for the given episode. If the episode is liked, it will be
  *  removed, otherwise it will be added.
  * @param like The id of the episode whose like is to be toggled.
  */
 export function toggleLike(like: Episode) {
+  const user = supabaseClient.auth.user();
+
+  if (user === null && !get(hasReceivedSignInSnackbarStore)) {
+    snackbar({
+      text: 'Your likes will only be visible on this device. To synchronize your favorite podcasts, please sign in.',
+      buttonText: 'Sign in',
+      callback: () => {
+        const credentials = { provider: 'github' } as const;
+        supabaseClient.auth.signIn(credentials, { redirectTo: window.location.href });
+      },
+    });
+    hasReceivedSignInSnackbarStore.set(true);
+  }
   const { id } = like;
   likesStore.update((likes) => {
     if (likes.has(id)) {
