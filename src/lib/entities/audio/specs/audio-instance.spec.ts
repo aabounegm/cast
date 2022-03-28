d4d0677fe082e9d6a6a171a422f963764b904bf0
alkaitagi <alkaitagi@outlook.com>
2022-03-28 19:00:45 +0300

    Replace binding with direct writable set

--- src/lib/entities/audio/specs/audio-instance.spec.ts
+++ src/lib/entities/audio/specs/audio-instance.spec.ts
@@ -1,22 +1,14 @@
-jest.mock('svelte/store', () => ({
-  ...jest.requireActual('svelte/store'),
-}));
-
 import { get } from 'svelte/store';
-
-jest.mock('$lib/entities/audio', () => ({
-  ...jest.requireActual('$lib/entities/audio'),
-  duration: {
-    subscribe: jest.fn(() => jest.fn()),
-  },
-}));
 
 import { src, duration, currentTime, seek, move, play, pause, paused } from '$lib/entities/audio';
 
 describe('Audio playback API', () => {
   const defaultSrc = 'http://commondatastorage.googleapis.com/codeskulptor-assets/Evillaugh.ogg';
   const defaultDuration = 6.452245;
-  const playDefault = () => play(defaultSrc);
+  const playDefault = () => {
+    play(defaultSrc);
+    duration.set(defaultDuration);
+  };
 
   it('loads audio', () => {
     playDefault();
@@ -36,25 +28,18 @@ describe('Audio playback API', () => {
 
   it('controls playback current time', () => {
     playDefault();
-    jest.mocked(duration.subscribe).mockImplementation((subscriber) => {
-      subscriber(defaultDuration);
-      return jest.fn();
-    });
-    // console.log('DD', get(duration));
     seek(0.5);
     move(2);
-    // console.log('CT', get(currentTime));
     expect(get(currentTime)).toEqual(2.5);
     pause();
   });
 
   it('sets invalid playback time', () => {
-    // playDefault();
-    // await new Promise((r) => setTimeout(r, 1000));
-    // seek(-1);
-    // expect(get(currentTime)).toEqual(0);
-    // move(100);
-    // expect(get(currentTime)).toEqual(get(duration));
-    // pause();
+    playDefault();
+    seek(-1);
+    expect(get(currentTime)).toEqual(0);
+    move(100);
+    expect(get(currentTime)).toEqual(get(duration));
+    pause();
   });
 });
