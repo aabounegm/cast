5166d5d04355abb0c6c8a72741a9150fac65d8b5
Lev Chelyadinov <leva181777@gmail.com>
2022-04-02 00:15:49 +0500

    Hide the stack trace from the Podcast Gallery page

--- src/lib/pages/podcast-gallery/lib/load-podcasts.ts
+++ src/lib/pages/podcast-gallery/lib/load-podcasts.ts
@@ -4,9 +4,12 @@ import { podcastList, podcasts } from '$lib/entities/podcast';
 export const loadPodcasts: Load = async () => {
   try {
     podcasts.set(await podcastList());
-  } catch (e) {
-    console.error(e);
+    return {};
+  } catch {
+    return {
+      status: 500,
+      error:
+        'This must be hard to be satisfied with. Reach the developers at GitHub (aabounegm/cast) and they will do their best to resolve this!',
+    };
   }
-
-  return {};
 };
