f8f748c62baac2465ffbe7a65dbc63bebec238eb
Abdelrahman Abounegm <abounegm.abdelrahman@gmail.com>
2022-04-01 20:52:10 +0300

    Attempt adding a test for `startEpisodeDownload`

--- src/lib/features/download-episode/lib/specs/download.spec.ts
+++ src/lib/features/download-episode/lib/specs/download.spec.ts
@@ -1,7 +1,27 @@
+import { get } from 'svelte/store';
 import { startEpisodeDownload } from '../download';
 
-it.skip('starts downloading episodes', () => {
-  startEpisodeDownload;
-  // I honestly don't know how to test this function
-  //  do we even need to test it?
+const sleep = (ms: number) => new Promise<void>((resolve) => setTimeout(resolve, ms));
+
+const mockFetchAndWatchProgress = jest
+  .fn()
+  .mockName('fetchAndWatchProgress() from $lib/features/download-episode/lib')
+  .mockImplementation(async (url, store) => {
+    store.set(0);
+    await sleep(1000);
+    store.set(100);
+  });
+
+jest.mock('../download', () => ({
+  ...jest.requireActual('../download'),
+  fetchAndWatchProgress: () => mockFetchAndWatchProgress,
+}));
+
+it.skip('starts downloading episodes and watches progress', async () => {
+  const exampleUrl = 'https://example.com';
+  const store = startEpisodeDownload(exampleUrl);
+  expect(mockFetchAndWatchProgress).toHaveBeenCalledWith(`${exampleUrl}?download=true`, store);
+  expect(get(store)).toBe(0);
+  await sleep(1200);
+  expect(get(store)).toBe(1000);
 });
