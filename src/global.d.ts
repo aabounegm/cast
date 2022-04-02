10a17e8f14ceeebfbb6c558e2ff0a0706ed02360
Lev Chelyadinov <leva181777@gmail.com>
2022-04-02 22:53:08 +0500

    Test the ID parameter in the URL for injections

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,6 +1,12 @@
 /// <reference types="@sveltejs/kit" />
 /// <reference types="unplugin-icons/types/svelte" />
 /// <reference types="vite/client" />
+/// <reference types="jest-extended" />
+
+declare module 'blns' {
+  let naughtyStrings: string[];
+  export default naughtyStrings;
+}
 
 interface ImportMetaEnv {
   readonly VITE_SUPABASE_APP_URL: string;
