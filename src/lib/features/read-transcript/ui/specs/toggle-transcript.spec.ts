91fdb9bb5d23f80b2d7bded3e89df5327f11b55d
Khaled Ismaeel <k.ismaeel@innopolis.university>
2022-04-07 00:08:22 +0300

    Update toggle-transcript.spec.ts

--- src/lib/features/read-transcript/ui/specs/toggle-transcript.spec.ts
+++ src/lib/features/read-transcript/ui/specs/toggle-transcript.spec.ts
@@ -1,5 +1,7 @@
 export {};
 
-it.todo('displays the current transcript state to the user');
+it.todo('when transcript is shown, shows correct text to the user')
+
+it.todo('when transcript is hidden, shows correct text to the user')
 
 it.todo('dispatches the click event to the handler');
