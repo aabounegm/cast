c6235288cf2a551e4387d8b5c164b61208a875b5
Khaled Ismaeel <k.ismaeel@innopolis.university>
2022-04-07 00:14:24 +0300

    Check if `EpisodeDisplay` and `FullControls` are correctly integrated

--- src/lib/widgets/now-playing/ui/specs/now-playing.spec.ts
+++ src/lib/widgets/now-playing/ui/specs/now-playing.spec.ts
@@ -3,3 +3,7 @@ export {};
 it.todo('initially contains episode display');
 
 it.todo('toggles between episode display and transcript');
+
+it.todo('displays the correct currently playing episode');
+
+it.todo('provides the user with playback controls');
