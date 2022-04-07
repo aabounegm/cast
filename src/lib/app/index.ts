3587c2755eb96733fd2ddb9b0bb53ae1f5fadcc5
Lev Chelyadinov <leva181777@gmail.com>
2022-04-07 20:54:07 +0500

    Configure auto-generation for meta images

--- src/lib/app/index.ts
+++ src/lib/app/index.ts
@@ -1,1 +1,3 @@
 export { handleHook } from './lib/handle-hook';
+export { default as MetaImages } from './meta/meta-images.svelte';
+export { buildManifest } from './meta/pwa-manifest';
---
+++ src/lib/app/meta/pwa-manifest.ts
@@ -0,0 +1,46 @@
+import { base } from '$app/paths';
+import appIconSvg from './app-icon.svg';
+import appIconPng192 from './app-icon.svg?width=192&png';
+import appIconPng512 from './app-icon.svg?width=512&png';
+import type { WebAppManifest } from 'web-app-manifest';
+import type { RequestHandler } from '@sveltejs/kit';
+
+const manifest: WebAppManifest = {
+  /* eslint-disable camelcase */
+  name: 'Cast',
+  short_name: 'Cast',
+  description: 'Listen to podcasts on the web, wherever!',
+  start_url: base,
+  background_color: '#1E293B',
+  theme_color: '#6366F1',
+  display: 'standalone',
+  orientation: 'portrait',
+  lang: 'en',
+  icons: [
+    {
+      src: appIconSvg,
+      type: 'image/svg+xml',
+      sizes: '512x512',
+    },
+    {
+      src: appIconPng192,
+      type: 'image/png',
+      sizes: '192x192',
+    },
+    {
+      src: appIconPng512,
+      type: 'image/png',
+      sizes: '512x512',
+    },
+  ],
+  /* eslint-enable camelcase */
+};
+
+/**
+ * Construct a PWA manifest.
+ *
+ * For details, see https://web.dev/add-manifest/
+ */
+export const buildManifest: RequestHandler = () => ({
+  body: JSON.stringify(manifest),
+});
