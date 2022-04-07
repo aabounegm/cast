4399f4daf9f7448d820440ff8d31b36f28bf35d7
alkaitagi <alkaitagi@outlook.com>
2022-04-07 12:44:50 +0300

    Add episodes caching

--- src/lib/pages/library/lib/get-episode.ts
+++ src/lib/pages/library/lib/get-episode.ts
@@ -1,11 +1,18 @@
-import { supabaseClient } from '$lib/shared/api';
+import { supabaseClient, type Episode } from '$lib/shared/api';
 import { transformEpisodeRequest, type SBEpisode } from '$lib/shared/api/adapters';
 
+const cache = new Map<number, Episode>();
+
 export async function getEpisode(id: number) {
+  if (cache.has(id)) return cache.get(id);
+
   const { data } = await supabaseClient
     .from<SBEpisode>('episodes')
     .select('*')
     .eq('id', id)
     .single();
-  return data ? transformEpisodeRequest(data) : null;
+
+  const episode = data ? transformEpisodeRequest(data) : null;
+  if (episode) cache.set(id, episode);
+  return episode;
 }
