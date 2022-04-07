6c656958940b6a6d131062a87493f9ef499ed781
Abdelrahman Abounegm <abounegm.abdelrahman@gmail.com>
2022-04-07 18:13:37 +0300

    Modify test to use login command

--- cypress/integration/auth.spec.ts
+++ cypress/integration/auth.spec.ts
@@ -1,4 +1,6 @@
-it('has a password to use', () => {
-  const password: string = Cypress.env('testUserPassword');
-  expect(password).to.be.a('string');
+/* eslint-disable testing-library/prefer-screen-queries, testing-library/await-async-query  */
+it('can log in with the test account', () => {
+  cy.login();
+  cy.visitAndWaitForHydration('/library');
+  cy.findByText('Signed in as', { exact: false }).should('exist');
 });
