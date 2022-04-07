72b67b903f5b24822d9e9d0276803dc43cb15e71
Abdelrahman Abounegm <abounegm.abdelrahman@gmail.com>
2022-04-07 18:12:29 +0300

    Add a Cypress command for log in

--- cypress/plugins/index.ts
+++ cypress/plugins/index.ts
@@ -4,6 +4,8 @@ dotenv.config();
 const config: Cypress.PluginConfig = (on, config) => {
   // copy any needed variables from process.env to config.env
   config.env.testUserPassword = process.env.TEST_USER_PASSWORD;
+  config.env.supabaseAppUrl = process.env.VITE_SUPABASE_APP_URL;
+  config.env.supabasePublicAnonKey = process.env.VITE_SUPABASE_PUBLIC_ANON_KEY;
 
   // do not forget to return the changed config object!
   return config;
