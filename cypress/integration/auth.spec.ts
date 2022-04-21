66d90b3720c7fac40a5a1e287d4f477d05abd24f
Abdelrahman Abounegm <abounegm.abdelrahman@gmail.com>
2022-04-21 14:14:39 +0300

    Turn off ESLint false positives in Cypress

--- cypress/integration/auth.spec.ts
+++ cypress/integration/auth.spec.ts
@@ -1,4 +1,3 @@
-/* eslint-disable testing-library/prefer-screen-queries, testing-library/await-async-query  */
 it('can log in with the test account', () => {
   cy.login();
   cy.visitAndWaitForHydration('/library');
