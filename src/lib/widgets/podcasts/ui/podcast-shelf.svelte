e2e2d28aea5cbe2a8c9d7f07f5e98946bbb37c7f
Lev Chelyadinov <leva181777@gmail.com>
2022-04-28 17:51:31 +0300

    Implement the empty state for the podcast shelf

--- src/lib/widgets/podcasts/ui/podcast-shelf.svelte
+++ src/lib/widgets/podcasts/ui/podcast-shelf.svelte
@@ -11,9 +11,19 @@
   {#if title !== undefined}
     <H1 class="pl-8 pt-8">{title}</H1>
   {/if}
-  <div class="flex overflow-x-auto snap-x snap-proximity pl-6 scroll-pl-6">
-    {#each podcasts as podcast (podcast.id)}
-      <PodcastTile class="snap-start" large {podcast} />
-    {/each}
-  </div>
+  {#if podcasts.length > 0}
+    <div class="flex overflow-x-auto snap-x snap-proximity pl-6 scroll-pl-6">
+      {#each podcasts as podcast (podcast.id)}
+        <PodcastTile class="snap-start" large {podcast} />
+      {/each}
+    </div>
+  {:else}
+    <div class="empty-shelf flex justify-center items-center">Nothing here yet</div>
+  {/if}
 </section>
+
+<style>
+  .empty-shelf {
+    height: 172px;
+  }
+</style>
