87565100273801331aeebdc7fc29a7db88d2d258
Lev Chelyadinov <leva181777@gmail.com>
2022-05-02 13:24:56 +0300

    Prevent syncing when the user is not signed in

--- src/lib/features/listening-history/api/history-table.ts
+++ src/lib/features/listening-history/api/history-table.ts
@@ -1,17 +1,11 @@
 import { supabaseClient } from '$lib/shared/api';
 
-/**
- * Function that adds a podcast id into `history` SB table.
- */
+/** Adds a podcast ID into the `history` table on Supabase. */
 export async function addToCloudListeningHistory(podcastId: number) {
-  try {
-    await supabaseClient.from('history').insert([
-      {
-        // eslint-disable-next-line camelcase
-        podcast_id: podcastId,
-      },
-    ]);
-  } catch {
-    throw new Error('Server error. Check internet connection and ensure you are signed in.');
+  // eslint-disable-next-line camelcase
+  const response = await supabaseClient.from('history').insert([{ podcast_id: podcastId }]);
+
+  if (response.status >= 400) {
+    throw response;
   }
 }
