d0a26091bd19f96c9780046fc3a49e173b3c365b
Lev Chelyadinov <leva181777@gmail.com>
2022-05-03 14:19:12 +0300

    Make the listening history reactive

--- src/lib/pages/podcast-gallery/ui/podcast-gallery-page.svelte
+++ src/lib/pages/podcast-gallery/ui/podcast-gallery-page.svelte
@@ -1,15 +1,17 @@
 <script lang="ts">
   import { PodcastShelf, PodcastList } from '$lib/widgets/podcasts';
+  import { listeningHistory } from '$lib/features/listening-history';
   import { podcasts, trendingPodcasts } from '$lib/entities/podcast';
+  import { notNull } from '$lib/shared/api';
 
-  import { loadListeningHistory } from '../model/load-listening-history';
-
-  const listeningHistory = loadListeningHistory();
+  $: podcastsInHistory = $listeningHistory
+    .map((thisID) => $podcasts.find((podcast) => podcast.id === thisID) ?? null)
+    .filter(notNull);
 </script>
 
 <div class="bg-slate-800">
-  {#if listeningHistory.length > 0}
-    <PodcastShelf title="Recently listened" podcasts={listeningHistory} id="recently-listened" />
+  {#if podcastsInHistory.length > 0}
+    <PodcastShelf title="Recently listened" podcasts={podcastsInHistory} id="recently-listened" />
   {/if}
 
   {#if $trendingPodcasts.length > 0}
