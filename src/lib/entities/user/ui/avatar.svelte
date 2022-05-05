6287672a3acea2bbcd0a59e2b543a708eab6ce2c
Lev Chelyadinov <leva181777@gmail.com>
2022-05-05 14:44:37 +0300

    Prevent the Avatar component erroring on the testing user

--- src/lib/entities/user/ui/avatar.svelte
+++ src/lib/entities/user/ui/avatar.svelte
@@ -1,6 +1,12 @@
 <script lang="ts">
   import { Image } from '$lib/shared/ui';
-  export let image: string;
+  export let image: string | undefined;
+
+  const emptyImage = 'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg"/>';
 </script>
 
-<Image class="aspect-square h-12 w-12 rounded-full overflow-hidden block" src={image} alt="" />
+{#if image === undefined}
+  <img class="aspect-square h-12 w-12 rounded-full overflow-hidden block" src={emptyImage} alt="" />
+{:else}
+  <Image class="aspect-square h-12 w-12 rounded-full overflow-hidden block" src={image} alt="" />
+{/if}
