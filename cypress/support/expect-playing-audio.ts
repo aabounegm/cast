a285581dca4a64ddb661a8ac75357367e0f94e64
Lev Chelyadinov <leva181777@gmail.com>
2022-05-07 16:00:31 +0300

    Add a command to check that no audio is playing

--- cypress/support/expect-playing-audio.ts
+++ cypress/support/expect-playing-audio.ts
@@ -1,5 +1,5 @@
-const expectPlayingAudio = () => {
-  cy.get('audio').should((els) => {
+export const expectPlayingAudio = () => {
+  return cy.get('audio').should((els) => {
     let audible = false;
     els.each((i, el) => {
       if (el.duration > 0 && !el.paused && !el.muted) {
@@ -10,4 +10,14 @@ const expectPlayingAudio = () => {
   });
 };
 
-export default expectPlayingAudio;
+export const expectNoPlayingAudio = () => {
+  return cy.get('audio').should((els) => {
+    let audible = false;
+    els.each((i, el) => {
+      if (el.duration > 0 && !el.paused && !el.muted) {
+        audible = true;
+      }
+    });
+    expect(audible).to.be.false;
+  });
+};
