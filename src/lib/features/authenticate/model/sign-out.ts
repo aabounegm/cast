bd8fc73fbd98ddfbea73112d6fdbc42413c7d46a
Abdelrahman Abounegm <abounegm.abdelrahman@gmail.com>
2022-05-08 17:27:23 +0300

    Clear likes and listening history on sign out
    Closes #237

--- src/lib/features/authenticate/model/sign-out.ts
+++ src/lib/features/authenticate/model/sign-out.ts
@@ -1,5 +1,9 @@
 import { supabaseClient } from '$lib/shared/api';
+import { likesStore } from '$lib/features/like-episode';
+import { listeningHistory } from '$lib/features/listening-history';
 
-export function signOut() {
-  supabaseClient.auth.signOut();
+export async function signOut() {
+  await supabaseClient.auth.signOut();
+  likesStore.set([]);
+  listeningHistory.set([]);
 }
