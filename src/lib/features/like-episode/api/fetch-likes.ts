87412fba8d6509acb590ff7c457891e1453f54af
Abdelrahman Abounegm <abounegm.abdelrahman@gmail.com>
2022-05-09 15:13:17 +0300

    Add missing type parameter to fix inference

--- src/lib/features/like-episode/api/fetch-likes.ts
+++ src/lib/features/like-episode/api/fetch-likes.ts
@@ -1,8 +1,9 @@
 import { supabaseClient } from '$lib/shared/api';
+import type { SBFavourite } from '$lib/shared/api/adapters';
 
 export async function fetchLikes() {
   const likes = await supabaseClient
-    .from('favourites')
+    .from<SBFavourite>('favourites')
     .select('episode_id')
     .order('created_at', { ascending: false });
   return likes.data?.map((e) => e.episode_id);
