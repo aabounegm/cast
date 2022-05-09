34739c10c7a457b0ccaeb959630821f72bbed1a6
Lev Chelyadinov <leva181777@gmail.com>
2022-05-10 01:31:35 +0300

    Try increasing the test coverage for the H1 component

--- src/lib/shared/ui/specs/h1.spec.ts
+++ src/lib/shared/ui/specs/h1.spec.ts
@@ -12,3 +12,19 @@ it('renders the text that was passed to it', () => {
 
   expect(screen.getByText(sampleContent)).toBeInTheDocument();
 });
+
+it('passes a custom class and other props to the DOM node', () => {
+  const sampleContent = 'sample content';
+  const sampleClass = 'class';
+  const sampleID = 'id';
+
+  const h1WithContent = useLocalVars(
+    svelte`<H1 class={sampleClass} id={sampleID}>{sampleContent}</H1>`,
+    [H1, sampleClass, sampleID, sampleContent]
+  );
+  render(h1WithContent);
+
+  const h1Node = screen.getByRole('heading');
+  expect(h1Node).toHaveAttribute('id', sampleID);
+  expect(h1Node).toHaveClass(sampleClass);
+});
