e86df827e385b429cec48f5cfc02b6bd9747ed6c
Abdelrahman Abounegm <abounegm.abdelrahman@gmail.com>
2022-05-09 15:49:05 +0300

    Replace user store with same supabase subscription
    to respect FSD

--- src/lib/features/authenticate/model/sign-out.ts
+++ src/lib/features/authenticate/model/sign-out.ts
@@ -1,9 +1,5 @@
 import { supabaseClient } from '$lib/shared/api';
-import { likesStore } from '$lib/features/like-episode';
-import { listeningHistory } from '$lib/features/listening-history';
 
 export async function signOut() {
   await supabaseClient.auth.signOut();
-  likesStore.set([]);
-  listeningHistory.set([]);
 }
